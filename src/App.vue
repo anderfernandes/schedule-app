cb1c7b9f774211802d38f043a60db3ab63ded002
anderfernandes <handerson171@gmail.com>
2020-08-29 14:47:19 -0500

    Made dist directory binary, updated date range to 14 days

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,39 @@
 <template>
-  <div class="ui fluid container">
+  <div>
     <div class="ui basic segment">
       <div class="ui blue icon message">
         <i class="info circle icon"></i>
         <div class="content">
           <div class="header">We now have an app!</div>
-          <p>
-            <a href="/go-beyond/app">Click here</a> to download it!
-          </p>
+          <p><a href="/go-beyond/app">Click here</a> to download it!</p>
         </div>
       </div>
       <div class="ui blue icon message">
         <i class="info circle icon"></i>
         <div class="content">
-          <div
-            class="header"
-          >Are you planning on visiting us after {{ format(new Date(end), 'EEEE, MMMM d') }}?</div>
+          <div class="header">
+            Are you planning on visiting us after
+            {{ format(new Date(end), 'EEEE, MMMM d') }}?
+          </div>
           <p>
-            <a href="/public-calendar">Click here</a> to see our Public Calendar, which contains all our future confirmed events as they are added.
+            <a href="/public-calendar">Click here</a> to see our Public
+            Calendar, which contains all our future confirmed events as they are
+            added.
           </p>
         </div>
       </div>
     </div>
-    <div class="ui basic segment" v-for="(day, i) in days" :key="i" v-show="day.events.length > 0">
+    <div
+      class="ui basic segment"
+      v-for="(day, i) in days"
+      :key="i"
+      v-show="day.events.length > 0"
+    >
       <div class="ui dividing header">
         <i class="calendar alternate outline icon"></i>
-        <div class="content">{{ format(new Date(day.date), 'EEEE, MMMM d') }}</div>
+        <div class="content">
+          {{ format(new Date(day.date), 'EEEE, MMMM d') }}
+        </div>
       </div>
       <div class="ui divided items" v-if="!loading">
         <event-box :event="event" v-for="event in day.events" :key="event.id" />
@@ -38,7 +46,10 @@
           target="_blank"
           class="ui black tiny image label"
         >
-          <img src="https://astral.anderfernandes.com/assets/astral-logo-light.png" alt />
+          <img
+            src="https://astral.anderfernandes.com/assets/astral-logo-light.png"
+            alt
+          />
           Powered by Astral
         </a>
       </div>
@@ -48,58 +59,58 @@
 </template>
 
 <script>
-import EventBox from "./components/EventBox.vue";
-import { addDays, format } from "date-fns";
-import axios from "axios";
+  import EventBox from './components/EventBox.vue'
+  import { addDays, format } from 'date-fns'
+  import axios from 'axios'
 
-export default {
-  data: () => ({
-    days: [],
-    loading: true,
-    end: addDays(new Date(), 7),
-    cover: null
-  }),
-  components: { EventBox },
-  methods: {
-    async fetchEvents() {
-      const today = new Date();
-      const start = format(today, "yyyy-MM-dd");
-      const end = format(this.end, "yyyy-MM-dd");
-      try {
-        const response = await axios.get(
-          `${SERVER}/api/events/by-date?start=${start}&end=${end}`
-        );
-        Object.assign(this, { days: response.data.data });
-        Object.assign(this, { loading: false });
-      } catch (error) {
-        console.log(error.message);
-      }
+  export default {
+    data: () => ({
+      days: [],
+      loading: true,
+      end: addDays(new Date(), 14),
+      cover: null,
+    }),
+    components: { EventBox },
+    methods: {
+      async fetchEvents() {
+        const today = new Date()
+        const start = format(today, 'yyyy-MM-dd')
+        const end = format(this.end, 'yyyy-MM-dd')
+        try {
+          const response = await axios.get(
+            `${SERVER}/api/events/by-date?start=${start}&end=${end}`
+          )
+          Object.assign(this, { days: response.data.data })
+          Object.assign(this, { loading: false })
+        } catch (error) {
+          console.log(error.message)
+        }
+      },
+      async fetchCover() {
+        try {
+          const response = await axios.get(
+            'https://astral.ctcd.org/api/settings'
+          )
+          Object.assign(this, {
+            cover: `https://astral.ctcd.org/storage/${response.data.cover}`,
+          })
+        } catch (error) {
+          console.log(error.message)
+        }
+      },
+      format,
     },
-    async fetchCover() {
-      try {
-        const response = await axios.get(
-          "https://astral.ctcd.org/api/settings"
-        );
-        Object.assign(this, {
-          cover: `https://astral.ctcd.org/storage/${response.data.cover}`
-        });
-      } catch (error) {
-        console.log(error.message);
-      }
+    async mounted() {
+      await this.fetchCover()
+      await this.fetchEvents()
     },
-    format
-  },
-  async mounted() {
-    await this.fetchCover();
-    await this.fetchEvents();
   }
-};
 </script>
 <style>
-.ui.embed > .placeholder {
-  width: inherit !important;
-  transform: translate(-50%, -50%) !important;
-  left: 50%;
-  top: 50%;
-}
+  .ui.embed > .placeholder {
+    width: inherit !important;
+    transform: translate(-50%, -50%) !important;
+    left: 50%;
+    top: 50%;
+  }
 </style>
